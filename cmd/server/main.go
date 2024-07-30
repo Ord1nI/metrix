cb193ad820f017592cc79217dbc70bb50718c49e
Ord1nI <ordinandrej@gmail.com>
2024-07-30 03:42:59 +0300

    add add GET hendlers & page with all metrics

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,71 @@
 package main
 
 import (
+    "github.com/go-chi/chi/v5"
+
     "net/http"
     "github.com/Ord1nI/metrix/cmd/handlers"
     "github.com/Ord1nI/metrix/cmd/storage"
 )
 
 
+func updateGaugeRoute(stor storage.RepositoriesAdder) func(r chi.Router){
+    return func(r chi.Router) {
+        r.HandleFunc("/", handlers.NotFound)                    // ANY /update/gauge/
+        r.Post("/{name}/{val}", handlers.UpdateGauge(stor))     // POST /update/gauge/name/123
+        r.HandleFunc("/{name}/{val}/*", handlers.BadRequest)    // ANY /update/gauge/name/123/adsf
+    }
+}
+
+func updateCounterRoute(stor storage.RepositoriesAdder) func(r chi.Router){
+    return func(r chi.Router) {
+        r.HandleFunc("/", handlers.NotFound)                    // ANY /update/gauge/
+        r.Post("/{name}/{val}", handlers.UpdateCounter(stor))     // POST /update/gauge/name/123
+        r.HandleFunc("/{name}/{val}/*", handlers.BadRequest)  // ANY /update/gauge/name/123/adsf
+    }
+}
+func valueGaugeRoute(stor storage.RepositoriesGetter) func(r chi.Router){
+    return func(r chi.Router) {
+        r.HandleFunc("/", handlers.NotFound)                //ANY /value/gauge/
+        r.Get("/{name}", handlers.GetGauge(stor))           //GET /value/gauge/name
+        r.HandleFunc("/{name}/*", handlers.BadRequest)      //ANY /value/gauge/name/asa
+    }
+}
+func valueCounterRoute(stor storage.RepositoriesGetter) func(r chi.Router){
+    return func(r chi.Router) {
+        r.HandleFunc("/", handlers.NotFound)                //ANY /value/counter/
+        r.Get("/{name}", handlers.GetCounter(stor))         //GET /value/counter/name
+        r.HandleFunc("/{name}/*", handlers.BadRequest)      //ANY /value/counter/name/qew
+    }
+}
 
 func main() {
 
+    r := chi.NewRouter()
+
     stor := storage.NewEmptyStorage()
 
-    mux := http.NewServeMux()
-    mux.HandleFunc(`/`, func(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusNotFound)})
-    mux.HandleFunc(`/update/`, func(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusBadRequest)})
-    mux.HandleFunc(`/update/gauge/`, handlers.UpdateGauge(stor))
-    mux.HandleFunc(`/update/counter/`, handlers.UpdateCounter(stor))
+    r.Get("/", handlers.WriteAllMetrics(stor))                  //POST localhost:/
 
-    err := http.ListenAndServe(`:8080`, mux)
+    r.Route("/update", func(r chi.Router) {
+        r.HandleFunc("/", handlers.BadRequest)                      // ANY /update/
+
+        r.Route("/gauge", updateGaugeRoute(stor))         // ANY /update/gauge/*
+
+        r.Route("/counter", updateCounterRoute(stor))     // Any /update/counter/*
+        
+    })
+
+    r.Route("/value", func(r chi.Router) {
+        r.Get("/", handlers.BadRequest)
+
+        r.Route("/gauge", valueGaugeRoute(stor))         // ANY /update/gauge/*
+
+        r.Route("/counter", valueCounterRoute(stor))     // Any /update/counter/*
+
+    })
+
+    err := http.ListenAndServe(`:8080`, r)
     if err != nil {
         panic(err)
     }
--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -1,23 +1,23 @@
 package main
 
 import (
+    "github.com/go-chi/chi/v5"
+
     "io"
+    "errors"
     "testing"
     "net/http"
     "net/http/httptest"
+
     "github.com/Ord1nI/metrix/cmd/handlers"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-type want struct {
-    code int
-    res string
-    val float64
-}
 
 type storageMock struct{
     val float64
+    name string
 }
 
 func newSM() *storageMock{
@@ -34,20 +34,60 @@ func (s *storageMock) AddCounter(name string, val int64) {
     s.val = float64(val)
 }
 
+func (s *storageMock) GetGauge(name string) (float64, error){
+    if s.name == name {
+        return s.val, nil
+    }
+    return 0, errors.New("error")
+}
+
+func (s *storageMock) GetCounter(name string) (int64, error){
+    if s.name == name {
+        return int64(s.val), nil
+    }
+    return 0, errors.New("error")
+}
+
 func TestMain(t *testing.T) {
     stor := newSM()
 
-    mux := http.NewServeMux()
-    mux.HandleFunc(`/`, func(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusNotFound)})
-    mux.HandleFunc(`/update/`, func(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusBadRequest)})
-    mux.HandleFunc(`/update/gauge/`, handlers.UpdateGauge(stor))
-    mux.HandleFunc(`/update/counter/`, handlers.UpdateCounter(stor))
+    r := chi.NewRouter()
 
-    serv := httptest.NewServer(mux)
+    r.Route("/update", func(r chi.Router) {
+        r.HandleFunc("/", handlers.BadRequest)                      // ANY /update/
+
+        r.Route("/gauge", updateGaugeRoute(stor))         // ANY /update/gauge/*
+
+        r.Route("/counter", updateCounterRoute(stor))     // Any /update/counter/*
+        
+    })
+
+    r.Route("/value", func(r chi.Router) {
+        r.Get("/", handlers.BadRequest)
+
+        r.Route("/gauge", valueGaugeRoute(stor))         // ANY /update/gauge/*
+
+        r.Route("/counter", valueCounterRoute(stor))     // Any /update/counter/*
+
+    })
+
+    serv := httptest.NewServer(r)
     client := serv.Client()
 
+    tCounter(t, stor, serv, client)
+    tGauge(t, stor, serv, client)
+    tCounterGet(t, stor, serv, client)
+    tGaugeGet(t, stor, serv, client)
+
+    
+}
+func tCounter(t *testing.T, stor *storageMock, serv *httptest.Server, client *http.Client){
+     
+    type want struct {
+        code int
+        res string
+        val float64
+    }
 
     tests := []struct{
         URL string
@@ -57,14 +97,106 @@ func TestMain(t *testing.T) {
             URL: "",
             want: want{
                 code:http.StatusNotFound,
-                res: "",
+                res: "404 page not found\n",
             },
         },
         {
             URL: "/update",
             want: want{
                 code:http.StatusBadRequest,
+                res: "Bad Request\n",
+            },
+        },
+        {
+            URL: "/update/counter/",
+            want: want{
+                code:http.StatusNotFound,
+                res: "Not Found\n",
+            },
+        },
+        {
+            URL: "/update/counter/name/123/asdf/",
+            want: want{
+                code:http.StatusBadRequest,
+                res: "Bad Request\n",
+            },
+        },
+        {
+            URL: "/update/counter/name/ads",
+            want: want{
+                code:http.StatusBadRequest,
+                res: "Incorect metric value\n",
+            },
+        },
+        {
+            URL: "/update/counter/name/123.213",
+            want: want{
+                code:http.StatusBadRequest,
+                res: "Incorect metric value\n",
+                val: 0,
+            },
+        },
+        {
+            URL: "/update/counter/name1/-123",
+            want: want{
+                code:http.StatusOK,
                 res: "",
+                val: -123,
+            },
+        },
+        {
+            URL: "/update/counter/name1/123",
+            want: want{
+                code:http.StatusOK,
+                res: "",
+                val: 123,
+            },
+        },
+    }
+    for _, test := range tests {
+        t.Run("testCounter " + serv.URL+test.URL,func(t *testing.T) {
+            stor.val = 0
+
+            res, err := client.Post(serv.URL+test.URL,"text/plain",nil)
+
+            require.NoError(t,err)
+
+            assert.Equal(t, test.want.code, res.StatusCode)
+
+            r, _ := io.ReadAll(res.Body)
+            res.Body.Close()
+            assert.Equal(t, test.want.res, string(r))
+            if stor == nil {
+                stor.val = 0
+            }
+            assert.Equal(t, test.want.val, stor.val)
+        })
+    }
+}
+func tGauge(t *testing.T, stor *storageMock, serv *httptest.Server, client *http.Client) {
+     
+    type want struct {
+        code int
+        res string
+        val float64
+    }
+
+    tests := []struct{
+        URL string
+        want want
+    }{
+        {
+            URL: "",
+            want: want{
+                code:http.StatusNotFound,
+                res: "404 page not found\n",
+            },
+        },
+        {
+            URL: "/update/",
+            want: want{
+                code:http.StatusBadRequest,
+                res: "Bad Request\n",
             },
         },
         {
@@ -121,91 +253,178 @@ func TestMain(t *testing.T) {
             if stor == nil {
                 stor.val = 0
             }
-            assert.Equal(t, stor.val, test.want.val)
+            assert.Equal(t, test.want.val, stor.val)
         })
     }
-    
-    tests = []struct{
+}
+func tCounterGet(t *testing.T, stor *storageMock, serv *httptest.Server, client *http.Client) {
+
+    type want struct {
+        code int
+        res string
+    }
+
+    tests := []struct{
         URL string
+        value int64
+        name string
         want want
     }{
         {
             URL: "",
             want: want{
                 code:http.StatusNotFound,
-                res: "",
+                res: "404 page not found\n",
             },
         },
         {
-            URL: "/update",
-            want: want{
-                code:http.StatusBadRequest,
-                res: "",
-            },
-        },
-        {
-            URL: "/update/counter/",
-            want: want{
-                code:http.StatusNotFound,
-                res: "Not Found\n",
-            },
-        },
-        {
-            URL: "/update/counter/name/123/asdf/",
+            URL: "/value/",
             want: want{
                 code:http.StatusBadRequest,
                 res: "Bad Request\n",
             },
         },
         {
-            URL: "/update/counter/name/ads",
+            URL: "/value/counter/",
             want: want{
-                code:http.StatusBadRequest,
-                res: "Incorect metric value\n",
+                code:http.StatusNotFound,
+                res: "Not Found\n",
             },
         },
         {
-            URL: "/update/counter/name/123.213",
+            URL: "/value/counter/name234",
+            name: "name",
             want: want{
-                code:http.StatusBadRequest,
-                res: "Incorect metric value\n",
-                val: 0,
+                code:http.StatusNotFound,
+                res: "Unknown metric\n",
             },
         },
         {
-            URL: "/update/counter/name1/-123",
+            URL: "/value/counter/name",
+            value: 233,
+            name: "name",
             want: want{
                 code:http.StatusOK,
-                res: "",
-                val: -123,
+                res: "233\n",
             },
         },
         {
-            URL: "/update/counter/name1/123",
+            URL: "/value/counter/name/dfs",
             want: want{
-                code:http.StatusOK,
-                res: "",
-                val: 123,
+                code:http.StatusBadRequest,
+                res: "Bad Request\n",
             },
         },
     }
     for _, test := range tests {
-        t.Run(serv.URL+test.URL,func(t *testing.T) {
-            stor.val = 0
+        t.Run(test.URL, func(t *testing.T) {
 
-            res, err := client.Post(serv.URL+test.URL,"text/plain",nil)
+            stor.val = float64(test.value)
+
+            stor.name = test.name
+
+            res, err := client.Get(serv.URL+test.URL)
 
             require.NoError(t,err)
 
             assert.Equal(t, test.want.code, res.StatusCode)
-
             r, _ := io.ReadAll(res.Body)
             res.Body.Close()
             assert.Equal(t, test.want.res, string(r))
-            if stor == nil {
-                stor.val = 0
-            }
-            assert.Equal(t, stor.val, test.want.val)
+        })
+    }
+}
+func tGaugeGet(t *testing.T, stor *storageMock, serv *httptest.Server, client *http.Client) {
+    type want struct {
+        code int
+        res string
+    }
+
+    tests := []struct{
+        URL string
+        value float64
+        name string
+        want want
+    }{
+        {
+            URL: "",
+            want: want{
+                code:http.StatusNotFound,
+                res: "404 page not found\n",
+            },
+        },
+        {
+            URL: "/value/",
+            want: want{
+                code:http.StatusBadRequest,
+                res: "Bad Request\n",
+            },
+        },
+        {
+            URL: "/value/gauge/",
+            want: want{
+                code:http.StatusNotFound,
+                res: "Not Found\n",
+            },
+        },
+        {
+            URL: "/value/gauge/name234",
+            name: "name",
+            want: want{
+                code:http.StatusNotFound,
+                res: "Unknown metric\n",
+            },
+        },
+        {
+            URL: "/value/gauge/name",
+            value: 233.213,
+            name: "name",
+            want: want{
+                code:http.StatusOK,
+                res: "233.213\n",
+            },
+        },
+        {
+            URL: "/value/gauge/name1",
+            value: 0,
+            name: "name1",
+            want: want{
+                code:http.StatusOK,
+                res: "0\n",
+            },
+        },
+        {
+            URL: "/value/gauge/name2",
+            value: -0,
+            name: "name2",
+            want: want{
+                code:http.StatusOK,
+                res: "0\n",
+            },
+        },
+        {
+            URL: "/value/gauge/name/dfs",
+            want: want{
+                code:http.StatusBadRequest,
+                res: "Bad Request\n",
+            },
+        },
+    }
+    for _, test := range tests {
+        t.Run(test.URL, func(t *testing.T) {
+
+            stor.val = test.value
+
+            stor.name = test.name
+
+            res, err := client.Get(serv.URL+test.URL)
+
+            require.NoError(t,err)
+
+            assert.Equal(t, test.want.code, res.StatusCode)
+            r, _ := io.ReadAll(res.Body)
+            res.Body.Close()
+            assert.Equal(t, test.want.res, string(r))
         })
     }
 }
