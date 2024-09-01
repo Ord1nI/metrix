83b41237ea92c24337af0ad8e8cc37ac25485edc
Ord1nI <ordinandrej@gmail.com>
2024-09-01 22:53:09 +0300

    test

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -211,59 +211,63 @@ func SingMW(l logger, key []byte) func(http.Handler) http.Handler{
     return func (handler http.Handler) http.Handler {
         f := func(w http.ResponseWriter, r *http.Request) {
             stringHash := r.Header.Get("HashSHA256")
-            if len(stringHash) < 64 {
-                l.Infoln("Bad hash")
-                w.WriteHeader(http.StatusBadRequest)
-                w.Write(nil)
-                return
+            if stringHash != "" {
+                if len(stringHash) < 64 {
+                    l.Infoln("Bad hash")
+                    w.WriteHeader(http.StatusNotFound)
+                    w.Write(nil)
+                    return
+                }
+
+                getHash, err := hex.DecodeString(stringHash)
+                if err != nil {
+                    l.Infoln("error whiele decoding hex", err)
+                    handlers.SendInternalError(w)
+                    return
+                }
+
+                bodyBytes, err := io.ReadAll(r.Body)
+
+                if err != nil {
+                    l.Infoln("error while reading body", err)
+                    handlers.SendInternalError(w)
+                    return
+                }
+
+                defer r.Body.Close()
+                r.Body = &ReqBody{
+                    Buffer: bytes.NewBuffer(bodyBytes),
+                }
+
+                signer := hmac.New(sha256.New, key)
+                _,err = signer.Write(bodyBytes)
+
+                if err != nil {
+                    l.Infoln("Error while signing")
+                    handlers.SendInternalError(w)
+                    return
+                }
+
+                Hash := signer.Sum(nil)
+
+                if !hmac.Equal(getHash, Hash) {
+                    l.Infoln("Hashes not equal")
+                    l.Infoln(getHash, "\n", Hash)
+                    w.WriteHeader(http.StatusBadRequest)
+                    w.Write(nil)
+                    return
+                }
+
+                signer.Reset()
+                srw := &sResponseWriter{w, signer}
+
+                l.Infoln("Request accepted")
+                handler.ServeHTTP(srw, r)
+
+                w.Header().Add("HashSHA256", hex.EncodeToString(srw.Signer.Sum(nil)))
+            } else {
+                handler.ServeHTTP(w,r)
             }
-
-            getHash, err := hex.DecodeString(stringHash)
-            if err != nil {
-                l.Infoln("error whiele decoding hex", err)
-                handlers.SendInternalError(w)
-                return
-            }
-
-            bodyBytes, err := io.ReadAll(r.Body)
-
-            if err != nil {
-                l.Infoln("error while reading body", err)
-                handlers.SendInternalError(w)
-                return
-            }
-
-            defer r.Body.Close()
-            r.Body = &ReqBody{
-                Buffer: bytes.NewBuffer(bodyBytes),
-            }
-
-            signer := hmac.New(sha256.New, key)
-            _,err = signer.Write(bodyBytes)
-
-            if err != nil {
-                l.Infoln("Error while signing")
-                handlers.SendInternalError(w)
-                return
-            }
-
-            Hash := signer.Sum(nil)
-
-            if !hmac.Equal(getHash, Hash) {
-                l.Infoln("Hashes not equal")
-                l.Infoln(getHash, "\n", Hash)
-                w.WriteHeader(http.StatusBadRequest)
-                w.Write(nil)
-                return
-            }
-
-            signer.Reset()
-            srw := &sResponseWriter{w, signer}
-
-            l.Infoln("Request accepted")
-            handler.ServeHTTP(srw, r)
-
-            w.Header().Add("HashSHA256", hex.EncodeToString(srw.Signer.Sum(nil)))
         }
         return http.HandlerFunc(f)
     }
